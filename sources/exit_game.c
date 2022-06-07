8813b762a2f1fc43d8df4f21a77ec408aaf1bc7f
Fatma Ozturk <93829110+Fatma-Ozturk@users.noreply.github.com>
2022-06-07 16:29:57 +0300

    so_long
    a small 2D game with minilibx

---
+++ sources/exit_game.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit_game.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: faozturk <faozturk@42kocaeli.tr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/26 12:29:21 by faozturk          #+#    #+#             */
+/*   Updated: 2022/04/26 12:29:41 by faozturk         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	free_map(char **map)
+{
+	int	i;
+
+	i = 0;
+	while (map[i] != NULL)
+	{
+		free(map[i]);
+		i++;
+	}
+	free(map);
+}
+
+int	exit_game(t_game *game)
+{
+	free_map(game->map);
+	mlx_destroy_image(game->mlx, game->img_backg);
+	mlx_destroy_image(game->mlx, game->img_wall);
+	mlx_destroy_image(game->mlx, game->img_player);
+	mlx_destroy_image(game->mlx, game->img_collect);
+	mlx_destroy_image(game->mlx, game->img_exit);
+	mlx_destroy_window(game->mlx, game->win);
+	free(game->mlx);
+	exit(0);
+	return (0);
+}
